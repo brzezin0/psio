4e6dd36a5c28656e3c3686740250b10f4430dd73
brzezin <236700@edu.p.lodz.pl>
2024-01-11 11:05:52 +0100

    Card detector improvements

--- main.py
+++ main.py
@@ -9,18 +9,29 @@ camera.open_camera()
 try:
     while True:
         frame = camera.get_frame()
+        # adjust threshold values
+        _, threshold = cv2.threshold(frame, 80, 255, cv2.THRESH_BINARY)
+        # adjust zoom
+        # focus  = frame[x1:x2,y1:y2]
 
-        cards = detector.detect_cards(frame)
-        for card in cards:
-            cv2.drawContours(frame, [card], -1, (0, 255, 0), 2)
+        # cards = detector.detect_cards(frame)
+        # for card in cards:
+        #     cv2.drawContours(frame, [card], -1, (0, 255, 0), 2)
 
-        grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Kamera', threshold)
 
-        cv2.imshow('Kamera', grayImage)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        key = cv2.waitKey(1)
+        if key == 27:
             break
 
 finally:
     camera.release_camera()
     cv2.destroyAllWindows()
+
+similarity_threshold = 0.8
+detector = CardDetector()
+is_card, card_image = detector.detect_playing_card('picture1', 'picture2', similarity_threshold)
+if is_card:
+    print("Wykryto kartę do gry.")
+else:
+    print("Nie wykryto karty do gry.")
